23127e42cbda8348f65f1155714c34a4c26fa73c
El0Ephant <75036763+El0Ephant@users.noreply.github.com>
2022-03-09 18:43:43 +0300

    Цветовая гамма, улучшенное расписание

--- lib/bottom bar/home/timetable/widgets/timetable_widget.dart
+++ lib/bottom bar/home/timetable/widgets/timetable_widget.dart
@@ -1,7 +1,10 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:i_student/provider/theme_provider/theme_provider.dart';
 import 'dart:io';
+
+import 'timetable_support.dart';
 
 class Timetable extends StatefulWidget {
 const Timetable({ Key? key }) : super(key: key);
@@ -15,104 +18,64 @@ _TimetableState createState() => _TimetableState();
 class _TimetableState extends State<Timetable> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ConstructDay(context, "ПОНЕДЕЛЬНИК"),
-        ConstructDay(context, "ВТОРНИК"),
-        ConstructDay(context, "СРЕДА"),
-        ConstructDay(context, "ЧЕТВЕРГ"),
-        ConstructDay(context, "ПЯТНИЦА"),
-        ConstructDay(context, "СУББОТА"),
-    ]
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      height: 400.0,
+      child: ListView.builder(
+        physics: ClampingScrollPhysics(),
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) => Container(
+          width: 300,
+          height: 150,
+          child: ConstructDay(context, days[index]),
+        ),
+      ),
     );
+
   }
 }
 
-Column ConstructDay(BuildContext context, String day) {
+Column ConstructDay(BuildContext context, Day day) {
   return Column(
-    children: [
-      Container(
-              margin: EdgeInsets.only(left: 20, top: 20),
-              child: Text(
-                day,
-                style: TextStyle(
-                    color: Theme
-                        .of(context)
-                        .hintColor,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w600),
-              )
-    ),
-
-Container(
-    margin: EdgeInsets.symmetric(vertical: 20),
-    height: 150.0,
-    child: ListView.builder(
-      physics: ClampingScrollPhysics(),
-      shrinkWrap: true,
-      scrollDirection: Axis.horizontal,
-      itemCount: 5,
-      itemBuilder: (BuildContext context, int index) => Container(
-        width: 300,
-        height: 150,
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25)),
-          ),
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                          margin: EdgeInsets.only(left: 5, bottom: 10),
-                          child: Text(
-                            'Предмет',
-                            style: TextStyle(
-                                color: Theme.of(context).hintColor,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400),
-                          )),
-
-                      Card(
-                        color: Theme.of(context).secondaryHeaderColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5)),
-                        ),
-                        child: Container(
-                            margin: EdgeInsets.symmetric(horizontal: 10),
-                            child: Text(
-                              'XX:XX - XX:XX',
-                              style: TextStyle(
-                                  color: Theme.of(context).cardColor,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500),
-                            )),
-                      ),
-
-                      Container(
-                          margin: EdgeInsets.only(left: 5, top: 5),
-                          child: Text(
-                            'Дополнительная информация',
-                            style: TextStyle(
-                                color: Theme.of(context).highlightColor,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w400),
-                          )),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
+      children: [
+        Container(
+            margin: EdgeInsets.only(left: 20, top: 20),
+            child: Text(
+              day.name,
+              style: TextStyle(
+                  color: Theme
+                      .of(context)
+                      .hintColor,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600),
+            )
         ),
-      ),
-    ),
-  ),
-  ],
+
+        ListView.builder(
+          itemCount: day.count,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Card(
+              color: MyThemes.lightTheme.primaryColor,
+                child: ListTile(
+              leading: Text(day.lessons[index].time),
+              title: Text(day.lessons[index].name,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              subtitle: Text(day.lessons[index].place,
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+
+            ),
+              );
+          },
+        )
+      ]
   );
 }
