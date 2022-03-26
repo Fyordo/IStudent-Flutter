a41efdba84b4f6aaac0b615491261678ec6068c8
Fyordo <lagutinfedya@gmail.com>
2022-03-26 17:35:00 +0300

    Добавил класс Student

---
+++ lib/model/Student.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+
+class Student{
+  late int id;
+  late String name;
+  late String email;
+  late int group_id;
+  late bool is_headman;
+  late List notifications;
+
+  Student(data){
+    id = data["id"];
+    name = data["name"];
+    email = data["email"];
+    group_id = data["group_id"];
+    is_headman = data["is_headman"];
+    notifications = data["notifications"];
+  }
+
+  info(){
+    String s = this.id.toString() + " " + this.name + " " + this.email;
+    print(s);
+  }
+}
