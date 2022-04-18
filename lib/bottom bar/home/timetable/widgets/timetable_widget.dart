9432b5f325732ac3b8dd7e92f05c557eb5c75da7
El0Ephant <75036763+El0Ephant@users.noreply.github.com>
2022-04-18 14:27:09 +0300

    Всплывающие окна в расписании

--- lib/bottom bar/home/timetable/widgets/timetable_widget.dart
+++ lib/bottom bar/home/timetable/widgets/timetable_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:i_student/data/Lesson.dart';
 import 'package:i_student/data/Schedule.dart';
-//import '../../../../data/structures.dart';
+
 
 const List<String> dayNames = [
   "Понедельник",
@@ -67,7 +67,6 @@ class ConstructDay extends StatelessWidget {
 
   List<Widget> _getPairs(List<Lesson> list, BuildContext context) {
     List<Widget> res = List<Widget>.empty(growable: true);
-
     res.add(Container(
         alignment: Alignment.center,
         margin: EdgeInsets.only(top: 20),
@@ -91,48 +90,120 @@ class ConstructDay extends StatelessWidget {
       if (list[index].location != "Online(Teams)") {
         clr = Color(0xffcdd3e5);
       }
-      res.add(Container(
-          margin: EdgeInsets.only(top: 10, left: 5, right: 5),
-          child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-              color: clr,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: clr,
-                  boxShadow: [
-                    BoxShadow(color: Colors.grey, spreadRadius: 2),
-                  ],
+      res.add(
+          GestureDetector(
+            onTap: () {
+              currentLesson = list[index];
+              Navigator.of(context).restorablePush(_pairDialogBuilder);
+              },
+            child: Container(
+            margin: EdgeInsets.only(top: 10, left: 5, right: 5),
+            child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
                 ),
-                child: ListTile(
-                  leading: Text(
-                    list[index].time,
-                    style: TextStyle(color: Colors.black, fontSize: 20),
+                color: clr,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: clr,
+                    boxShadow: [
+                      BoxShadow(color: Colors.grey, spreadRadius: 2),
+                    ],
                   ),
-                  title: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      17 > list[index].title.length
-                          ? list[index].title
-                          : list[index].title.substring(0, 17) + "...",
+                  child: ListTile(
+                    leading: Text(
+                      list[index].time,
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    title: Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Text(
+                        17 > list[index].title.length
+                            ? list[index].title
+                            : list[index].title.substring(0, 17) + "...",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    subtitle: Text(
+                      list[index].location,
                       style: TextStyle(
                           color: Colors.black,
-                          fontSize: 20,
+                          fontSize: 16,
                           fontWeight: FontWeight.w600),
                     ),
                   ),
-                  subtitle: Text(
-                    list[index].location,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ))));
+                ))),
+      ));
     }
     return res;
   }
+}
+
+Lesson? currentLesson;
+
+Route<Object?> _pairDialogBuilder(
+      BuildContext context, Object? arguments) {
+      return RawDialogRoute<void>(
+        pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            ) {
+              var title = currentLesson?.title ?? "...";
+              var time = currentLesson?.time ?? "......";
+              var teacher = currentLesson?.teacher.name ?? "...";
+
+              var label = TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600);
+              var value = TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600);
+
+              return SimpleDialog(
+                  title: Text(
+                    title,
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColorDark,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  children: <Widget>[
+                    SimpleDialogOption(
+                      child: Row(
+                                children: [
+                                  Text("время начала:", style: label),
+                                  Spacer(),
+                                  Text(time.substring(0, time.length ~/ 2), style: value),
+                                ],
+                              ),
+                      ),
+                    SimpleDialogOption(
+                      child: Row(
+                        children: [
+                          Text("время конца:", style: label),
+                          Spacer(),
+                          Text(time.substring(time.length ~/ 2, time.length), style: value),
+                        ],
+                      ),
+                    ),
+
+                    SimpleDialogOption(
+                      child: Text("ФИО преподавателя:", style: label),
+                    ),
+
+                    SimpleDialogOption(
+                      child: Text(teacher, style: value),
+                    ),
+                  ],
+              );
+              },
+      );
+
 }
